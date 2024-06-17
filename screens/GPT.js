e301eab4f34eff1f3d97eda6c4b3fdc7e113ef37
Brandon T <btai@bayviewglen.ca>
2024-06-17 11:16:34 -0400

    groq api

--- screens/GPT.js
+++ screens/GPT.js
@@ -1,16 +1,44 @@
 import React, { useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { config } from 'dotenv'
+
+import { GROQ_API_KEY } from '@env';
 // USE GROQ https://console.groq.com/playground
+
+import Groq from "groq-sdk";
+
+const groq = new Groq({ apiKey: GROQ_API_KEY, dangerouslyAllowBrowser: true});
+
+export async function main() {
+  const chatCompletion = await getGroqChatCompletion();
+  // Print the completion returned by the LLM.
+  console.log(chatCompletion.choices[0]?.message?.content || "");
+}
+
+export async function getGroqChatCompletion() {
+  return groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: "Explain the importance of fast language models",
+      },
+    ],
+    model: "llama3-8b-8192",
+  });
+}
+
+
 const GPT = () => {
 
     const fetch = () => {
-        console.log(process.env.API_KEY)
+        console.log( GROQ_API_KEY )
 
     }
 
+    
+
       useEffect(() => {
         fetch();
+        main()
     }, []);
     return (
         
